550d140558517fda303ce5d9f0ec00a2a86f1e12
Jyotiranjan767 <jyotiranjan767@gmail.com>
2021-10-13 00:49:34 +0530

    first commit

---
+++ src/containers/login/index.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+  email: string;
+  password: string;
+}
+
+const Login: React.FC<Props> = ({ email, password }) => {
+  return (
+    <div>
+
+    </div >
+  )
+}
+
+export default Login
